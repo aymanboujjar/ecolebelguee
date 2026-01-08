2ad27b119a1737f1722a6770ca800507b64be965
yahya-moussair <yahyamoussair05@gmail.com>
2026-01-08 23:29:47 +0100

    feat : create jsx pages for pourquoi menu dropdown

---
+++ resources/js/pages/pourquoi/avantages.jsx
@@ -0,0 +1,7 @@
+export default function Avantages() {
+    return (
+        <div>
+            <h1>Avantages</h1>
+        </div>
+    );
+}
---
+++ resources/js/pages/pourquoi/valeurs.jsx
@@ -0,0 +1,7 @@
+export default function Valeurs() {
+    return (
+        <div>
+            <h1>Valeurs</h1>
+        </div>
+    );
+}
